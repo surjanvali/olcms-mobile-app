5d99d80198152c591ecbbf6508e3301d72d9c3ac
bhanukota210 <bhanu.infy210@gmail.com>
2022-07-20 16:35:23 +0530

    Daily status for GP api responses

---
+++ src/in/apcfss/olcms/InstructionsStatusForGP.java
@@ -0,0 +1,280 @@
+package in.apcfss.olcms;
+
+import java.math.BigInteger;
+import java.net.URLEncoder;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import in.apcfss.util.ECourtsCryptoHelper;
+import in.apcfss.util.EHighCourtAPI;
+import in.apcfss.util.HASHHMACJava;
+import plugins.DatabasePlugin;
+
+/**
+ * @author : Bhanu Krishna Kota
+ * @title :
+ * 
+ *        PRD URL :
+ *        https://apolcms.ap.gov.in/apolcms-services/services/getInstructions/displayCasesList
+ *        TEST URL :
+ *        http://localhost:9090/apolcms-services/services/getInstructions/displayCasesList
+ * 
+ *        {"REQUEST" : {"USER_ID":"gp-ser2@ap.gov.in", "ROLE_ID":"6","DEPT_CODE":"0", "DIST_ID":"0"}} 
+ *        
+ **/
+
+@Path("/getInstructions")
+public class InstructionsStatusForGP {
+			
+	@POST
+	@Produces({ "application/json" })
+	@Consumes({ "application/json" })
+	@Path("/displayCasesList")
+	public static Response displayCasesListForInstructions(String incomingData) throws Exception {
+
+		Connection con = null;
+		String jsonStr = "";
+
+
+		try {
+			if (incomingData != null && !incomingData.toString().trim().equals("")) {
+				JSONObject jObject1 = new JSONObject(incomingData);
+
+				System.out.println("jObject1:" + jObject1);
+
+				JSONObject jObject = new JSONObject(jObject1.get("REQUEST").toString().trim());
+				System.out.println("jObject:" + jObject);
+
+				if (!jObject.has("USER_ID") || jObject.get("USER_ID").toString().equals("")) {
+					jsonStr = "{\"RESPONSE\" : {\"RSPCODE\" :\"00\"  ,  \"RSPDESC\" :\"Error:Mandatory parameter- USER_ID is missing in the request.\" }}";
+				} else if (!jObject.has("ROLE_ID") || jObject.get("ROLE_ID").toString().equals("")) {
+					jsonStr = "{\"RESPONSE\" : {\"RSPCODE\" :\"00\"  ,  \"RSPDESC\" :\"Error:Mandatory parameter- ROLE_ID is missing in the request.\" }}";
+				} else if (!jObject.has("DEPT_CODE") || jObject.get("DEPT_CODE").toString().equals("")) {
+					jsonStr = "{\"RESPONSE\" : {\"RSPCODE\" :\"00\"  ,  \"RSPDESC\" :\"Mandatory parameter- DEPT_CODE is missing in the request.\" }}";
+				} else if (!jObject.has("DIST_ID") || jObject.get("DIST_ID").toString().equals("")) {
+					jsonStr = "{\"RESPONSE\" : {\"RSPCODE\" :\"00\"  ,  \"RSPDESC\" :\"Mandatory parameter- DIST_ID is missing in the request.\" }}";
+				}  else {
+					String sql = null, sqlCondition = "", roleId="", distId="", deptCode="", userid="";
+					userid = jObject.get("USER_ID").toString();
+					roleId = jObject.get("ROLE_ID").toString();
+					deptCode = jObject.get("DEPT_CODE").toString();
+					distId = jObject.get("DIST_ID").toString();
+					con = DatabasePlugin.connect();
+					
+					
+					 sql="select type_name_reg, reg_no, reg_year, to_char(dt_regis,'dd-mm-yyyy') as dt_regis, a.cino, case when length(scanned_document_path) > 10 then scanned_document_path else '-' end as scanned_document_path "
+						 		+ " from (select distinct cino from ecourts_dept_instructions) a inner join ecourts_case_data d on (a.cino=d.cino)"
+						 		+ " where d.dept_code in (select dept_code from ecourts_mst_gp_dept_map where gp_id='"+userid+"')";
+						 
+						 
+						System.out.println("SQL:" + sql);
+						List<Map<String, Object>> data = DatabasePlugin.executeQuery(sql, con);
+						JSONArray finalList = new JSONArray();
+						
+					if (data != null && !data.isEmpty() && data.size() > 0) {
+						for (Map<String, Object> entry : data) {		
+						    
+					    	JSONObject cases = new JSONObject();
+					    	cases.put("case_type", entry.get("type_name_reg").toString());
+					    	String caseno = entry.get("type_name_reg").toString()+" "+entry.get("reg_no").toString()+"/"+entry.get("reg_year").toString();
+					    	cases.put("case_no", caseno);						    	
+					    	cases.put("case_reg_date", entry.get("dt_regis").toString());
+					    	cases.put("status", "Pending");
+					    	cases.put("cino", entry.get("cino").toString());
+					    	finalList.put(cases);
+					}
+					JSONObject casesData = new JSONObject();
+					casesData.put("CASES_LIST", finalList);
+					String finalString = casesData.toString();
+					    
+					jsonStr = "{\"RESPONSE\" : {\"RSPCODE\" :\"01\"  , \"RSPDESC\" :\"Cases retrived successfully\"  , "+finalString.substring(1,finalString.length()-1)+"}}";
+					
+						
+					} else {
+						jsonStr = "{\"RESPONSE\" : {\"RSPCODE\" :\"00\"  ,  \"RSPDESC\" :\"No Records Found.\" }}";
+					}				
+
+				}
+			} else {
+				jsonStr = "{\"RESPONSE\" : {\"RSPCODE\" :\"00\"  ,  \"RSPDESC\" :\"No Input Data.\" }}";
+			}
+		} catch (Exception e) {
+			jsonStr = "{\"RESPONSE\" : {\"RSPCODE\" :\"00\"  ,  \"RSPDESC\" :\"Error:Invalid Data.\" }}";
+			// conn.rollback();
+			e.printStackTrace();
+
+		} finally {
+			if (con != null)
+				con.close();
+		}
+		return Response.status(200).entity(jsonStr).build();
+
+	}
+	
+	@POST
+	@Produces({ "application/json" })
+	@Consumes({ "application/json" })
+	@Path("/submitDailyStatus")
+	public static Response submitInstructions(String incomingData) throws Exception {
+
+		Connection con = null;
+		String jsonStr = "";
+
+
+		try {
+			if (incomingData != null && !incomingData.toString().trim().equals("")) {
+				JSONObject jObject1 = new JSONObject(incomingData);
+
+				System.out.println("jObject1:" + jObject1);
+
+				JSONObject jObject = new JSONObject(jObject1.get("REQUEST").toString().trim());
+				System.out.println("jObject:" + jObject);
+
+				if (!jObject.has("USER_ID") || jObject.get("USER_ID").toString().equals("")) {
+					jsonStr = "{\"RESPONSE\" : {\"RSPCODE\" :\"00\"  ,  \"RSPDESC\" :\"Error:Mandatory parameter- USER_ID is missing in the request.\" }}";
+				} else if (!jObject.has("ROLE_ID") || jObject.get("ROLE_ID").toString().equals("")) {
+					jsonStr = "{\"RESPONSE\" : {\"RSPCODE\" :\"00\"  ,  \"RSPDESC\" :\"Error:Mandatory parameter- ROLE_ID is missing in the request.\" }}";
+				} else if (!jObject.has("DEPT_CODE") || jObject.get("DEPT_CODE").toString().equals("")) {
+					jsonStr = "{\"RESPONSE\" : {\"RSPCODE\" :\"00\"  ,  \"RSPDESC\" :\"Mandatory parameter- DEPT_CODE is missing in the request.\" }}";
+				} else if (!jObject.has("DIST_ID") || jObject.get("DIST_ID").toString().equals("")) {
+					jsonStr = "{\"RESPONSE\" : {\"RSPCODE\" :\"00\"  ,  \"RSPDESC\" :\"Mandatory parameter- DIST_ID is missing in the request.\" }}";
+				}  else if (!jObject.has("CINO") || jObject.get("CINO").toString().equals("")) {
+					jsonStr = "{\"RESPONSE\" : {\"RSPCODE\" :\"00\"  ,  \"RSPDESC\" :\"Mandatory parameter- CINO is missing in the request.\" }}";
+				}  else if (!jObject.has("DAILY_STATUS") || jObject.get("DAILY_STATUS").toString().equals("")) {
+					jsonStr = "{\"RESPONSE\" : {\"RSPCODE\" :\"00\"  ,  \"RSPDESC\" :\"Mandatory parameter- DAILY_STATUS is missing in the request.\" }}";
+				}  else {
+					String sql = null, sqlCondition = "", roleId="", distId="", deptCode="", userid="",cino="",daily_status="";
+					userid = jObject.get("USER_ID").toString();
+					roleId = jObject.get("ROLE_ID").toString();
+					deptCode = jObject.get("DEPT_CODE").toString();
+					distId = jObject.get("DIST_ID").toString();
+					cino=jObject.get("CINO").toString();
+					daily_status=jObject.get("DAILY_STATUS").toString();
+					con = DatabasePlugin.connect();
+					PreparedStatement ps = null;
+					
+					
+					sql = "insert into ecourts_gpo_daily_status (cino, status_remarks , dept_code ,dist_code,insert_by ) "
+							+ " values (?,?, ?, ?, ?)";
+
+					ps = con.prepareStatement(sql);
+					int i = 1;
+					ps.setString(i, cino);
+					ps.setString(++i, daily_status != null ? daily_status : "");
+					ps.setString(++i, deptCode);
+					ps.setString(++i, distId);
+					ps.setString(++i, userid);
+
+					System.out.println("sql--"+sql);
+
+					int a = ps.executeUpdate();
+					if(a>0) {
+						jsonStr = "{\"RESPONSE\" : {\"RSPCODE\" :\"01\"  ,  \"RSPDESC\" :\"Daily status details saved sucessfully\" }}";
+					}
+					else {
+						jsonStr = "{\"RESPONSE\" : {\"RSPCODE\" :\"00\"  ,  \"RSPDESC\" :\"Error in submission. Kindly try again.\" }}";
+					}
+
+				}
+			} else {
+				jsonStr = "{\"RESPONSE\" : {\"RSPCODE\" :\"00\"  ,  \"RSPDESC\" :\"No Input Data.\" }}";
+			}
+		} catch (Exception e) {
+			jsonStr = "{\"RESPONSE\" : {\"RSPCODE\" :\"00\"  ,  \"RSPDESC\" :\"Error:Invalid Data.\" }}";
+			// conn.rollback();
+			e.printStackTrace();
+
+		} finally {
+			if (con != null)
+				con.close();
+		}
+		return Response.status(200).entity(jsonStr).build();
+
+	}
+	
+	
+	@POST
+	@Produces({ "application/json" })
+	@Consumes({ "application/json" })
+	@Path("/getInstructionsHistory")
+	public static Response getInstructionsHistory(String incomingData) throws Exception {
+
+		Connection con = null;
+		String jsonStr = "";
+
+		try {
+			if (incomingData != null && !incomingData.toString().trim().equals("")) {
+				JSONObject jObject1 = new JSONObject(incomingData);
+
+				System.out.println("jObject1:" + jObject1);
+
+				JSONObject jObject = new JSONObject(jObject1.get("REQUEST").toString().trim());
+				System.out.println("jObject:" + jObject);
+
+				if (!jObject.has("CINO") || jObject.get("CINO").toString().equals("")) {
+					jsonStr = "{\"RESPONSE\" : {\"RSPCODE\" :\"00\"  ,  \"RSPDESC\" :\"Error:Mandatory parameter- CINO is missing in the request.\" }}";
+				}  else {
+					String sql = null, sqlCondition = "", roleId="", distId="", deptCode="", userid="",cino="",daily_status="";
+					
+					cino=jObject.get("CINO").toString();
+					con = DatabasePlugin.connect();
+					sql = "select status_remarks, to_char(insert_time,'dd-mm-yyyy HH:mi:ss') as insert_time from ecourts_gpo_daily_status where cino='" + cino + "'  order by 1 ";
+					System.out.println("sql--" + sql);
+					List<Map<String, Object>> instructionsHistory = DatabasePlugin.executeQuery(sql, con);
+					JSONArray finalList = new JSONArray();
+					
+					if (instructionsHistory != null && !instructionsHistory.isEmpty() && instructionsHistory.size() > 0) {
+						
+						for (Map<String, Object> entry : instructionsHistory) {		
+						    
+						    	JSONObject history = new JSONObject();
+						    	history.put("status_remarks", entry.get("status_remarks").toString());
+						    	history.put("submitted_time", entry.get("insert_time").toString());
+						    	
+						    	finalList.put(history);
+						}
+						JSONObject casesData = new JSONObject();
+						casesData.put("DAILY_STATUS_HISTORY", finalList);
+						String finalString = casesData.toString();
+						    
+						jsonStr = "{\"RESPONSE\" : {\"RSPCODE\" :\"01\"  , \"RSPDESC\" :\"Daily Status history retrived successfully\"  , "+finalString.substring(1,finalString.length()-1)+"}}";
+						
+					} else {
+						jsonStr = "{\"RESPONSE\" : {\"RSPCODE\" :\"00\"  ,  \"RSPDESC\" :\"No Records Found.\" }}";
+					}
+					
+					
+				}
+			} else {
+				jsonStr = "{\"RESPONSE\" : {\"RSPCODE\" :\"00\"  ,  \"RSPDESC\" :\"No Input Data.\" }}";
+			}
+		} catch (Exception e) {
+			jsonStr = "{\"RESPONSE\" : {\"RSPCODE\" :\"00\"  ,  \"RSPDESC\" :\"Error:Invalid Data.\" }}";
+			// conn.rollback();
+			e.printStackTrace();
+
+		} finally {
+			if (con != null)
+				con.close();
+		}
+		return Response.status(200).entity(jsonStr).build();
+
+	}
+	
+	
+}
+	
